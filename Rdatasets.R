32de22a5f53a9c3065a7a9cffa75cab16436a051
Vincent Arel-Bundock <varel@umich.edu>
2018-03-11 21:53:39 -0400

    temporary problem

--- Rdatasets.R
+++ Rdatasets.R
@@ -1,60 +1,86 @@
+# Utility packages
 library(pacman)
-library(R2HTML)
-packages = c("datasets", "boot", "DAAG", "gamclass", "KMsurv", "robustbase", "car", "cluster", "COUNT", "Ecdat", "gap", "ggplot2", "HistData", "lattice", "MASS", "plm", "plyr", "pscl", "reshape2", "rpart", "sandwich", "sem",  "survival", "vcd", "Zelig", "HSAUR", "psych", "quantreg", "geepack", "texmex", "multgee", "evir", "lme4", "mosaicData", "ISLR", "Stat2Data", "hwde", "mi", "mediation")
+p_load(R2HTML, tidyverse)
+
+# Data packages
+packages = c("datasets", "boot", "DAAG", "lmec", "gamclass", "KMsurv", "robustbase", "car", "cluster", "COUNT", "Ecdat", "gap", "ggplot2", "HistData", "lattice", "MASS", "plm", "plyr", "pscl", "reshape2", "rpart", "sandwich", "sem",  "survival", "vcd", "Zelig", "HSAUR", "psych", "quantreg", "geepack", "texmex", "multgee", "evir", "lme4", "mosaicData", "ISLR", "Stat2Data", "hwde", "mi", "mediation")
 p_load(char = packages)
+
+# Functions
+get_doc = function(package = 'mi', dataset = 'nlsyV') {
+    help.ref = try(help(eval(dataset), package=eval(package)), silent = TRUE)
+    out = try(utils:::.getHelpFile(help.ref), silent = TRUE)
+    return(out)
+}
+
+get_data = function(package = 'mi', dataset = 'nlsyV') {
+    e = new.env(hash = TRUE, parent = parent.frame(), size = 29L)
+    data(list = dataset, package = package, envir = e)
+    out = e[[dataset]]
+    return(out)
+}
+
+tidy_data = function(dat) {
+    if(class(dat)[1]=='ts'){
+        dat = try(data.frame('time' = time(dat), 'value' = dat), silent = TRUE)
+    } else {
+        dat = try(as.data.frame(dat), silent = TRUE)
+    }
+    if (class(dat)[1] == 'data.frame') {
+        out = dat
+    } else {
+        out = NA
+        class(out) = 'try-error'
+    }
+    return(out)
+}
+
+write_data = function(i) {
+    package = index$Package[i]
+    dataset = index$Item[i]
+    dat = data[[i]]
+    doc = docs[[i]]
+    cat(package, ' -- ', dataset, '\n')
+    try(dir.create('csv/'), silent = TRUE)
+    try(dir.create('doc/'), silent = TRUE)
+    try(dir.create(paste0('csv/', package)), silent = TRUE)
+    try(dir.create(paste0('doc/', package)), silent = TRUE)
+    fn_csv = paste0('csv/', package, '/', dataset, '.csv')
+    fn_doc = paste0('doc/', package, '/', dataset, '.html')
+    write.csv(data[[i]], file = fn_csv)
+    tools::Rd2HTML(docs[[i]], out = fn_doc)
+}
 
 # Index
 index = data(package=packages)$results[,c(1,3,4)]
 index = data.frame(index, stringsAsFactors=FALSE)
-index_out = NULL
-size_out = NULL
 
-# Save datasets
-for (i in 1:nrow(index)) {
-    dataset = index$Item[i]
-    package = index$Package[i]
-    # Load data in new environment (very hackish)
-    e = new.env(hash = TRUE, parent = parent.frame(), size = 29L)
-    cmd = paste('data(', dataset, ', envir=e)', sep='')
-    eval(parse(text=cmd))
-    d = e[[dataset]]
-    if(class(d) %in% c('data.frame', 'matrix', 'numeric', 'table', 'ts')){
-        cat("Processing data set: ", dataset, "\n")
-        if(class(d)=='ts'){
-            d = data.frame(time(d), d)
-            colnames(d) = c('time', dataset)
-        }
-        try(dir.create(paste('csv/', package, sep='')))
-        try(dir.create(paste('doc/', package, sep='')))
-        dest_csv = paste('csv/', package, '/', dataset, '.csv', sep='')
-        dest_doc = paste('doc/', package, '/', dataset, '.html', sep='')
-        # Save data as CSV
-        write.csv(d, dest_csv)
-        # Save documentation as HTML
-        help.ref = help(eval(dataset), package=eval(package))
-        help.file = utils:::.getHelpFile(help.ref)
-        tools::Rd2HTML(help.file, out=dest_doc)
-        # Add entry to index out
-        index_out = rbind(index_out, index[i,])
-        # Add entry to dimensions out
-        size_d = dim(d)
-        if (is.null(size_d)) size_d = c(length(d), 1)  # numeric vector
-        size_out = rbind(size_out, data.frame(Rows=size_d[1], Cols=size_d[2]))
-    }
-}
+# Extract Data and Docs and exclude non-data.frames and errors
+data = lapply(1:nrow(index), function(i) get_data(index$Package[i], index$Item[i]))
+docs = lapply(1:nrow(index), function(i) get_doc(index$Package[i], index$Item[i]))
+data = lapply(data, tidy_data)
+idx1 = sapply(docs, class) != 'try-error'
+idx2 = sapply(data, class) != 'try-error'
+idx = as.logical(pmin(idx1, idx2))
+data = data[idx]
+docs = docs[idx]
+index = index[idx,]
 
-# Size details
-index_out = cbind(index_out, size_out)
-# CSV index
-index_out$csv = paste('https://raw.github.com/vincentarelbundock/Rdatasets/master/csv/',
-                      index_out$Package, '/', index_out$Item, '.csv', sep='')
-index_out$doc = paste('https://raw.github.com/vincentarelbundock/Rdatasets/master/doc/',
-                      index_out$Package, '/', index_out$Item, '.html', sep='')
-write.csv(index_out, file='datasets.csv', row.names=FALSE)
+# Index
+index$Rows = sapply(data, nrow)
+index$Cols = sapply(data, ncol)
+index$CSV = paste('https://raw.github.com/vincentarelbundock/Rdatasets/master/csv/',
+                  index$Package, '/', index$Item, '.csv', sep='')
+index$Doc = paste('https://raw.github.com/vincentarelbundock/Rdatasets/master/doc/',
+                  index$Package, '/', index$Item, '.html', sep='')
+index = index[order(index$Package, index$Item),]
 
-# HTML index
-index_out$csv = paste("<a href='", index_out$csv, "'> CSV </a>", sep='')
-index_out$doc = paste("<a href='", index_out$doc, "'> DOC </a>", sep='')
+# Index CSV
+write.csv(index, file = 'datasets.csv', row.names = FALSE)
+
+# Index HTML
+index$CSV = paste("<a href='", index$CSV, "'> CSV </a>", sep='')
+index$Doc = paste("<a href='", index$Doc, "'> DOC </a>", sep='')
 unlink('datasets.html')
 rss = '
 <style type="text/css">
@@ -64,5 +90,9 @@ rss = '
 </style>
 '
 cat(rss, file='datasets.html')
-HTML(index_out, file='datasets.html', row.names=FALSE, append=TRUE)
+HTML(index, file='datasets.html', row.names=FALSE, append=TRUE)
 
+# Write to file
+for (i in 1:nrow(index)) {
+    write_data(i)
+}
